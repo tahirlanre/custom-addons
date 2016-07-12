a73a30cbc780c8e63bc248768c38e4272545374d
Tahir <tahirlanre@hotmail.com>
2016-07-12 06:57:00 +0100

    second commit

--- stock_report/report/stock_report.py
+++ stock_report/report/stock_report.py
@@ -1,9 +1,20 @@
-from openerp import models, fields
+from openerp.report import report_sxw
+from openerp.osv import osv
+from openerp.report.report_sxw import rml_parse
+import time
+import datetime
+from dateutil.relativedelta import relativedelta
+from operator import itemgetter
+from openerp.tools.float_utils import float_round
 
 
-class stock_report(models.Model):
-    _name = 'stock.report'
-    
-    
+
+class Parser(report_sxw.rml_parse):
     def test(self):
-        pass
+        print 'Hello World!'
+
+class report_stock_report(osv.AbstractModel):
+    _name = 'report.stock_report.stock_report_view'
+    _inherit = 'report.abstract_report'
+    _template = 'stock_report.stock_report_view'
+    _wrapped_report_class = Parser
--- stock_report/wizard/stock_report_wizard.py
+++ stock_report/wizard/stock_report_wizard.py
@@ -3,24 +3,35 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from openerp import api, fields, models
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+from openerp.tools.safe_eval import safe_eval as eval
+import openerp.addons.decimal_precision as dp
+from openerp.tools.float_utils import float_round
 
+import time
 
-class stock_report_wizard(models.TransientModel):
-    _name = 'stock.report.wizard'
+class stock_report_wizard(osv.osv_memory):
+    _name = 'stock.report'
     
-    from_date = fields.Date('From')
-    to_date = fields.Date('To')
-    location_id =fields.Many2one('stock.location', string='Location')
-   
-    @api.multi
-    def render_html(self, data=None):
-        report_obj = self.env['report']
-        report = report_obj._get_report_from_name('stock_report.stock_report_view')
-        docargs = {
-            'doc_ids': self._ids,
-            'doc_model': report.model,
-            'docs': self,
-        }
-        #print 'i got here'
-        return report_obj.render('stock_report.stock_report_view', docargs)
+    _columns = {
+        'from_date': fields.date("Date from", required=True),
+        'to_date': fields.date("Date", required=True),
+        'location_id': fields.many2one('stock.location', 'Location', domain="[('usage', '=', 'internal')]"),
+    }
+    _defaults = {
+               'to_date': lambda *a: time.strftime('%Y-%m-%d'),
+               'from_date': lambda *a: time.strftime('%Y-%m-%d'),
+               }
+    
+
+    def print_report(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        datas = {'ids': context.get('active_ids', [])}
+        res = self.read(cr, uid, ids, ['from_date', 'to_date', 'location_id'], context=context)
+        res = res and res[0] or {}
+        datas['form'] = res
+        if res.get('id',False):
+            datas['ids']=[res['id']]
+        return self.pool['report'].get_action(cr, uid, [], 'stock_report.stock_report_view', data=datas, context=context)
