f31355f2e3cc83b379369be1968369dcc3672e51
Tahir <tahirlanre@hotmail.com>
2017-03-12 12:12:59 +0100

    Update

--- pos_ejide/models/pos_ejide.py
+++ pos_ejide/models/pos_ejide.py
@@ -4,4 +4,4 @@ from openerp import models, fields, api
 class res_company(models.Model):
     _inherit = 'res.company' 
     
-    branch = fields.Char('Branch')
+    #branch = fields.Char('Branch')
