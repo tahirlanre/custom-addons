12d5e143edda94b2d8be56dd7a2bec33d1ff480e
Tahir <tahirlanre@hotmail.com>
2019-07-07 16:44:11 +0100

    receivable receivable/payable account balance row and in dispute column

--- account_financial_reports_usability/models.py
+++ account_financial_reports_usability/models.py
@@ -6,18 +6,3 @@ from openerp import models, fields, api
 #     _name = 'account_financial_reports_usability.account_financial_reports_usability'
 
 #     name = fields.Char()
-
-import time
-from lxml import etree
-
-from openerp.osv import fields, osv
-from openerp.osv.orm import setup_modifiers
-from openerp.tools.translate import _
-
-class account_common_report(osv.osv_memory):
-    _inherit = "account.common.report"
-    
-    
-    _defaults = {
-            'fiscalyear_id': False,
-    }
